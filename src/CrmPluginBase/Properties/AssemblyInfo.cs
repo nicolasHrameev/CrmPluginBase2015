480ede4571aa27adbcc9e291fd268096907c9098
abelevtsov <belevtsov80@mail.ru>
2016-07-09 14:19:29 +0300

    added missed file

---
+++ src/CrmPluginBase/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CrmPluginBase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CrmPluginBase")]
+[assembly: AssemblyCopyright("Copyright © Raresoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b8b15fb6-7537-4452-89fa-cf7042526ef6")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
